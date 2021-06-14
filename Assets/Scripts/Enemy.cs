02202e936c19940658f5db6bac759255913eb834
Rafael Nanes <rafaelbraga_22@hotmail.com>
2021-06-14 20:11:40 +0100

    Added explosion instantiation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
+    [SerializeField] GameObject ExplosionEffect;
+    [SerializeField] Transform Parent;
     private void OnParticleCollision(GameObject other)
     {
+        GameObject vfx = Instantiate(ExplosionEffect, transform.position, Quaternion.identity);
+        vfx.transform.parent = Parent.transform;
         Destroy(gameObject);
     }
-
-
 
 }
